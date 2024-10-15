a81474af46d0ce1911aa458ebb9b2bd23bb1bdc4
Tasneemhussain <tasneemhussain321@gmail.com>
2024-10-15 21:57:32 +0200

    added heading (frontend)

--- client/src/components/search/Search.tsx
+++ client/src/components/search/Search.tsx
@@ -4,7 +4,10 @@ const Search = () => {
     return (
         <div>
             {/* Tasneem SEARCH COMPONENT / USER INPUT */}
-            
+               <h1>Allah</h1>
+               <h2>  bismillahi qurli waqtarli </h2>
+               <h3> welcome to the Local Travel and weather Dashboard</h3>
+               <p> Enter your address to see public transport depatures, <br />Local weather,and Trafic incidents </p>
         </div>
     );
 };
